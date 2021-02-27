5477a41cb2f3e6c1ce874a90732cba95d5f3bbe8
Toni Vicente <arv187@users.noreply.github.com>
2021-02-27 22:33:39 +0100

    Create resumen.java

---
+++ resumen.java
@@ -0,0 +1,63 @@
+package com.resumen;
+
+public class Resumen1 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * 1. Fundamentos
+		 */
+		
+		// println
+		System.out.println("Hola a todo el mundo");
+		System.out.println("Hola a todo el mundo");
+		
+		// print
+		System.out.print("Hola a todo el mundo");
+		System.out.print("Hola a todo el mundo");
+		
+		// printf
+		System.out.printf("Hola a todo %d el mundo %f", 2, 3.2f).println();
+		
+		// escapar o saltos de línea o tabulaciones
+		System.out.println(" \"  \n   \t \' ");
+		
+		/*
+		 * comentario multilínea
+		 */
+		
+		// variable primitiva de tipo entero
+		int a = 4;
+		
+		// variables de tipo flotante
+		float b = 4.2f;
+		double c = 4.3;
+		
+		// variables de un carácter
+		char d = 'b';
+		
+		// variables de tipo booleano
+		boolean e = false;
+		
+		// operadores
+		System.out.println(3 + 3);
+		System.out.println(3 % 3);
+		
+		// operadores de asignación compuesto
+		int f = 5;
+		f += 2;
+		f -= 2;
+		
+		// operadores autoincremento
+		int g = 6;
+		g++;
+		
+		// operadores decremento
+		int h = 6;
+		h--;
+		
+		
+
+	}
+
+}
